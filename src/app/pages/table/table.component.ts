1fce85ca04dfaff8170111f8fae0eab46d71f87c
Juan Diego Lopez <juan.1701624716@ucaldas.edu.co>
2023-02-28 10:29:19 -0500

    Agregando estilo a las tablas

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -4,19 +4,22 @@ import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-table',
-  templateUrl: './table.component.html'
+  templateUrl: './table.component.html',
 })
 export class TableComponent implements OnInit {
-
-  products : Product[] = [];
+  products: Product[] = [];
   columns: string[] = ['id', 'title', 'price', 'cover'];
+  total = 0;
 
-
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) {}
   ngOnInit(): void {
-    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products').
-    subscribe(data => {
-      this.products = data;
-    })
-}
+    this.http
+      .get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data) => {
+        this.products = data;
+        this.total = this.products
+          .map((item) => item.price)
+          .reduce((price, total) => price + total, 0);
+      });
+  }
 }
