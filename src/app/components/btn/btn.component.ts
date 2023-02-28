cc9c04c9dd80a2fcb3cc869acae119ce16c8e700
Juan Diego Lopez <juan.1701624716@ucaldas.edu.co>
2023-02-28 11:46:09 -0500

    mejoras UX y reusabilidad

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -6,7 +6,41 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class BtnComponent implements OnInit {
   @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
-  @Input() color = 'primary';
+  // @Input() color: 'success' | 'danger' | 'primary' | 'light' | 'sky'= 'primary';
+  @Input() color:keyof typeof this.mapColors= 'success'
+
+  mapColors = {
+    success: {
+      'bg-success-500': true,
+      'hover:bg-success-600': true,
+      'focus:ring-success-3000': true,
+      'text-white': true,
+    },
+    primary: {
+      'bg-primary-500': true,
+      'hover:bg-primary-600': true,
+      'focus:ring-primary-3000': true,
+      'text-white': true,
+    },
+    danger: {
+      'bg-danger-500': true,
+      'hover:bg-danger-600': true,
+      'focus:ring-danger-3000': true,
+      'text-white': true,
+    },
+    light: {
+      'bg-gray-200': true,
+      'hover:bg-gray-500': true,
+      'focus:ring-gray-50': true,
+      'text-gray-700': true,
+    },
+    sky: {
+      'bg-sky-700': true,
+      'hover:bg-sky-800': true,
+      'focus:ring-sky-300': true,
+      'text-white': true,
+    }
+  }
 
   constructor() {}
 
@@ -14,21 +48,11 @@ export class BtnComponent implements OnInit {
 
 
   get colors(){
-    return{
-      'text-white': this.color === 'success' || this.color === 'primary' || this.color === 'red',
-      'text-gray-700': this.color === 'gray-light',
-      'bg-success-500': this.color === 'success',
-      'hover:bg-success-600': this.color === 'success',
-      'focus:ring-success-3000': this.color === 'success',
-      'bg-primary-500': this.color === 'primary',
-      'hover:bg-primary-600':this.color === 'primary',
-      'focus:ring-primary-3000': this.color === 'primary',
-      'bg-red-500': this.color === 'red',
-      'hover:bg-red-600':this.color === 'red',
-      'focus:ring-red-3000': this.color === 'red',
-      'bg-gray-200': this.color === 'gray-light',
-      'hover:bg-gray-500':this.color === 'gray-light',
-      'focus:ring-gray-50': this.color === 'gray-light',
+    const colors = this.mapColors[this.color];
+    if(colors){
+      return colors;
     }
+    return {};
+
   }
 }
